0c55096fcbb2865ee2b2d2015ba1373183879e3f
thiago.concrete <thiago.concrete@corp.globo.com>
2017-08-30 19:25:28 -0300

    rota

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var router = require('express').Router();
-
+var client = require("../config/client");
 
 router.get('/', function(request, response){
     console.log("passou");
 response.send('ALOU FUNCIONOU');
 });
 
+router.get("/usuario/:globoID",function(request, response){
+       client.getUsuario(request.params.globoID, response);
+ });
 
 
 module.exports = router;
