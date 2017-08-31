fc3b3dfedb6dd0040acf8fe2f6ae87ed6c733e2f
Josue Concrete <josue.concrete@corp.globo.com>
2017-08-31 11:11:24 -0300

    refactor

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
-var express = require('express'), 
-http = require("http"),
-bodyParser = require("body-parser")
-client = require("./config/client");
-routes = require("./routes");
+var   express = require('express'), 
+      client = require("./config/client");
+      routes = require("./routes");
 
 
 var app = express();
 
-app.use(bodyParser.json());
+
 app.use(routes);
 
 
-prop = require("./config/properties");
-app.get("/token", function(request, response){
-response.send("teste");
+var server = app.listen(4000, function() {
+      console.log("Servidor rodando na porta 4000...");
 });
-
-
-app.get("/usuario/:globoID",function(request, response){
-       client.getUsuario(request.params.globoID, response);
- });
-
-
-var server = app.listen(4000, function(){
-console.log("Servidor rodando na porta 4000");
-});
-
-
-
-
-
 
 
--- config/client.js
+++ config/client.js
@@ -9,95 +9,93 @@ function msgInfo(mensagem, codigo) {
     this.codigo = codigo;
 }
 
-
-function doGet(rota,responseCallBack){
+function doGet(rota, responseCallBack) {
     objRequisicao = {
-        url : rota, 
-        headers: { 
-        "Authorization" : "Bearer " + token_atual}};
-request.get(objRequisicao, function(error, resp, body) {
-    if(!error){
-     console.log("Requisicao GET  efetuada para : " + objRequisicao.url + " Retorno = " + resp.statusCode);
-    }else{
-        console.log("Erro ao efetuar Requisicao para : " + objRequisicao.url + " ERROR: " + error);
-    }
+        url: rota,
+        headers: {
+            "Authorization": "Bearer " + token_atual
+        }
+    };
+    request.get(objRequisicao, function (error, resp, body) {
+        if (!error) {
+            console.log("Requisicao GET  efetuada para : " + objRequisicao.url + " Retorno = " + resp.statusCode);
+        } else {
+            console.log("Erro ao efetuar Requisicao para : " + objRequisicao.url + " ERROR: " + error);
+        }
 
-console.log(resp.headers.via);
+        console.log(resp.headers.via);
 
-       if(!error && resp.statusCode / 100 == 2){
-          var retorno = JSON.parse(body);
-         setTimeout(function () {
-               responseCallBack.send(traduzUsuario(retorno));
-        }, 200);
-       }else if (resp.statusCode == 401){
+        if (!error && resp.statusCode / 100 == 2) {
+            var retorno = JSON.parse(body);
+
+            responseCallBack.send(traduzUsuario(retorno));
+        } else if (resp.statusCode == 401) {
             getToken();
-            setTimeout(function () {
-               doGet(rota,responseCallBack);
-        }, 400);
-         
-       }else if(resp.statusCode == 403){
-           if(resp.headers.via && resp.headers.via.includes("GATEWAY response")){
-               responseCallBack.send(new msgInfo("Erro no acesso a rota no backstage", 403));
-           }else{
-               responseCallBack.send(new msgInfo("Usuario não encontrado", 403));
-           }
-         
-       }
-    })
+            doGet(rota, responseCallBack);
+        } else if (resp.statusCode == 403) {
+            if (resp.headers.via && resp.headers.via.includes("GATEWAY response")) {
+                responseCallBack.send(new msgInfo("Erro no acesso a rota no backstage", 403));
+            } else {
+                responseCallBack.send(new msgInfo("Usuario não encontrado", 403));
+            }
+
+        }
+
+    });
 }
-    
 
 
-function getToken(){
-request.post({url: prop.token_url + prop.grant_type, headers: prop.header_auth},function(error, response, body){
-var token = JSON.parse(body);
-token_atual = token.access_token;
-})};
+function getToken() {
+    request.post({ url: prop.token_url + prop.grant_type, headers: prop.header_auth }, function (error, response, body) {
+        var token = JSON.parse(body);
+        token_atual = token.access_token;
+    })
+};
 
-function getUsuarioGlive(globoID ,responseCallBack){
-   var rota = prop.get_usuario_url + globoID;
+function getUsuarioGlive(globoID, responseCallBack) {
+    var rota = prop.get_usuario_url + globoID;
     doGet(rota, responseCallBack);
 
 };
 
-function getUsuarioPorUsername(username, responseCallBack){
+function getUsuarioPorUsername(username, responseCallBack) {
     var rota = prop.get_usuario_username_url + username;
     doGet(rota, responseCallBack);
 
-   
+
 };
 
-function getUsuarioPorEmail(login, responseCallBack){
+function getUsuarioPorEmail(login, responseCallBack) {
     var rota = prop.get_usuario_email_url + login;
-     doGet(rota, responseCallBack);
+    doGet(rota, responseCallBack);
 
 };
 
 
-function traduzUsuario(gliveResponse){
+function traduzUsuario(gliveResponse) {
 
-return  usuarioTraduzido = {
-                nome : gliveResponse.fullName,
-                email : gliveResponse.email,
-                login : gliveResponse.username,
-                globoID : gliveResponse.id,
-                endereco : { 
-                    pais : gliveResponse.address.country.name,
-                    estado : gliveResponse.address.state.name,
-                    cidade: gliveResponse.address.city.name,
-                    bairro: gliveResponse.address.neighborhood,
-                    rua: gliveResponse.address.address1,
-                    numero: gliveResponse.address.number,
-                    complemento: gliveResponse.address.address2
-                }
-            };
+    return usuarioTraduzido = {
+        nome: gliveResponse.fullName,
+        email: gliveResponse.email,
+        login: gliveResponse.username,
+        globoID: gliveResponse.id,
+        endereco: {
+            pais: gliveResponse.address.country.name,
+            estado: gliveResponse.address.state.name,
+            cidade: gliveResponse.address.city.name,
+            bairro: gliveResponse.address.neighborhood,
+            rua: gliveResponse.address.address1,
+            numero: gliveResponse.address.number,
+            complemento: gliveResponse.address.address2
+        }
+    };
 
 }
 
-module.exports= {
-    getUsuario : getUsuarioGlive,
-    getUsuarioPorUsername : getUsuarioPorUsername,
-    getUsuarioPorEmail : getUsuarioPorEmail
+module.exports = {
+    getUsuario: getUsuarioGlive,
+    getUsuarioPorUsername: getUsuarioPorUsername,
+    getUsuarioPorEmail: getUsuarioPorEmail
 }
 
 
